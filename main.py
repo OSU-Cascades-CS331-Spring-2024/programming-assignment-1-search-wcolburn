6f708a8069b0f73d1272848be6d6bca98194400d
Liam Colburn <colburnw@oregonstate.edu>
2024-04-09 19:08:50 -0700

    Create basic loop to parse text

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
 import json
 
 file = open("france.txt")
-file.readline()
+
+
+text = file.read().replace('\n', ' ').split()
+print(text)
+
+i = 0
+city = ""
+next_city = ""
+distance = ""
+while i < len(text):
+    city = text[i]
+    print(city)
+    i += 10
+
+    while text[i][0] == 'v' and text[i][1] == 'a':
+        if text[i] == "va-":
+            i += 1
+        next_city = text[i]
+        i += 1
+        distance = text[i]
+        print(next_city, distance)
+        i += 1
+        if i >= len(text):
+            break
+
+
+
 file.close()
