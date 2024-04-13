634dbf47719592cc9933bd433f41fef032294bd9
Liam Colburn <colburnw@oregonstate.edu>
2024-04-12 17:19:28 -0700

    Add city class and use file to populate map

--- main.py
+++ main.py
@@ -1,31 +1,38 @@
-import json
+from city import City
+from map import Map
 
 file = open("france.txt")
 
-
 text = file.read().replace('\n', ' ').split()
-print(text)
+
+map = Map()
 
 i = 0
 city = ""
 next_city = ""
 distance = ""
 while i < len(text):
-    city = text[i]
-    print(city)
-    i += 10
+
+    city_name = text[i]
+    city = City(city_name)
+    map.add_city(city)
+
+    i += 10  # Skip coordinates
 
     while text[i][0] == 'v' and text[i][1] == 'a':
-        if text[i] == "va-":
+        if text[i] == "va-":  # In case of line breaking
             i += 1
-        next_city = text[i]
+        next_city_name = text[i].replace('va-', '', 1)
+        next_city = City(next_city_name)
         i += 1
-        distance = text[i]
-        print(next_city, distance)
+        distance = int(text[i])
+
+        map.add_city(next_city)
+        map.add_distance(city, next_city, distance)
+
+        # print(next_city.name, distance)
         i += 1
         if i >= len(text):
             break
-
-
 
 file.close()
--- map.py
+++ map.py
@@ -1,11 +1,14 @@
 class Map:
-    def __init__(self, cities):
+    def __init__(self, cities=None):
+        if cities is None:
+            cities = []
         self.cities = cities
-        self.distance = {}
+        self.distances = {}
 
-        def add_distance(city1, city2, distance):
-            self.distance[city1] = [city2, distance]
-            self.distance[city2] = [city1, distance]
+    def add_distance(self, city1, city2, distance):
+        # print(distance)
+        self.distances[city1] = [city2, distance]
+        self.distances[city2] = [city1, distance]
 
-        def add_city(city):
-            self.cities.append(city)
+    def add_city(self, city):
+        self.cities.append(city)
