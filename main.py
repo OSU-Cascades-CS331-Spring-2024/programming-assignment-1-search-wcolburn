ccdb83cded3fb9032bcf18003450623f8cbfafcb
Liam Colburn <colburnw@oregonstate.edu>
2024-04-12 19:42:09 -0700

    Refactor file parsing into new function

--- main.py
+++ main.py
@@ -10,45 +10,47 @@ p.add_argument("-B", help="End city", required=False)
 p.add_argument("-f", "--file", help="File to load map from", required=True)
 
 
-def main(**kwargs):
-    print(kwargs["file"])
+def create_map_from_file(file_name):
+    file = open(file_name)
 
+    text = file.read().replace('\n', ' ').split()
 
-file = open("france.txt")
+    new_map = Map()
 
-text = file.read().replace('\n', ' ').split()
+    i = 0
+    while i < len(text):
 
-map = Map()
+        city_name = text[i]
+        city = City(city_name)
+        new_map.add_city(city)
 
-i = 0
-city = ""
-next_city = ""
-distance = ""
-while i < len(text):
+        i += 10  # Skip coordinates
 
-    city_name = text[i]
-    city = City(city_name)
-    map.add_city(city)
-
-    i += 10  # Skip coordinates
-
-    while text[i][0] == 'v' and text[i][1] == 'a':
-        if text[i] == "va-":  # In case of line breaking
+        while text[i][0] == 'v' and text[i][1] == 'a':
+            if text[i] == "va-":  # In case of line breaking
+                i += 1
+            next_city_name = text[i].replace('va-', '', 1)
+            next_city = City(next_city_name)
             i += 1
-        next_city_name = text[i].replace('va-', '', 1)
-        next_city = City(next_city_name)
-        i += 1
-        distance = int(text[i])
+            distance = int(text[i])
 
-        map.add_city(next_city)
-        map.add_distance(city, next_city, distance)
+            new_map.add_city(next_city)
+            new_map.add_distance(city, next_city, distance)
 
-        # print(next_city.name, distance)
-        i += 1
-        if i >= len(text):
-            break
+            # print(next_city.name, distance)
+            i += 1
+            if i >= len(text):
+                break
+    file.close()
+    return new_map
 
-file.close()
+
+def main(**kwargs):
+    file_name = kwargs["file"]
+    map = create_map_from_file(file_name)
+    for city in map.cities:
+        print(city.name)
+
 
 if __name__ == '__main__':
     args = p.parse_args()
