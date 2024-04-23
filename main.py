0b8cb89fc9f015475521010b0587e4449f2fc35c
Liam Colburn <colburnw@oregonstate.edu>
2024-04-22 17:01:43 -0700

    Compute when no cities are given

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from map import Map
 from agent import Agent
 import argparse
+import sys
 
 
 def convert_dms_to_dd(coordinates):
@@ -21,7 +22,7 @@ def create_map_from_file(file_name):
     new_map = Map()
     i = 0
     while i < len(text):
-        city = text[i]
+        city = text[i].lower()
         new_map.add_city(city)
 
         dms_longitude = []
@@ -40,7 +41,7 @@ def create_map_from_file(file_name):
         while text[i][0] == 'v' and text[i][1] == 'a':
             if text[i] == "va-":  # In case of line breaking
                 i += 1
-            next_city = text[i].replace('va-', '', 1)
+            next_city = text[i].replace('va-', '', 1).lower()
             i += 1
             distance = int(text[i])
             new_map.add_distance(city, next_city, distance)
@@ -62,11 +63,35 @@ def print_path(path):
 
 
 def print_results(result):
+    if result["path"] == "failure":
+        print("Path failure")
+        return
     print_path(result["path"])
     print("Total cost: " + str(result["cost"]))
     print("Number of nodes explored: " + str(result["explored"]))
     print("Number of nodes expanded: " + str(result["expanded"]))
     print("Number of nodes maintained: " + str(result["maintained"]))
+
+
+def print_results_to_solution_file(results):
+    # Source: https://www.blog.pythonlibrary.org/2016/06/16/python-101-redirecting-stdout/
+    original = sys.stdout
+    sys.stdout = open('solutions.txt', 'a')
+    print("Solutions for traveling from", results[0]["path"][0], "to", results[0]["path"][-1])
+    print("----------------------------------------------------")
+    for i in range(0, 4):
+        result = results[i]
+        if i == 0:
+            print("BFS:")
+        elif i == 1:
+            print("DLS:")
+        elif i == 2:
+            print("UCS:")
+        else:
+            print("AStar:")
+        print_results(result)
+        print()
+    sys.stdout = original
 
 
 def main(**kwargs):
@@ -75,10 +100,10 @@ def main(**kwargs):
 
     agent = Agent()
 
-    if kwargs["A"] and kwargs["B"]:
-        search_type = kwargs["search"]
-        city_a = kwargs["A"]
-        city_b = kwargs["B"]
+    if kwargs["A"] and kwargs["B"]:  # If user specifies a start city and an end city
+        search_type = kwargs["search"].lower()
+        city_a = kwargs["A"].lower()
+        city_b = kwargs["B"].lower()
 
         result = dict()
         if search_type == "bfs":
@@ -90,6 +115,19 @@ def main(**kwargs):
         elif search_type == "astar":
             result = agent.astar(map, city_a, city_b)
         print_results(result)
+    else:  # Search over default 9 cities with each search method
+        cities_to_search = [("brest", "nice"), ("montpellier", "calais"), ("strasbourg", "bordeaux"),
+                            ("paris", "grenoble"), ("grenoble", "paris"), ("brest", "grenoble"), ("grenoble", "brest"),
+                            ("nice", "nantes"), ("caen", "strasbourg")]
+        for city_pair in cities_to_search:
+            city_a = city_pair[0]
+            city_b = city_pair[1]
+            bfs_result = agent.bfs(map, city_a, city_b)
+            dls_result = agent.iterative_deepening_search(map, city_a, city_b)
+            ucs_result = agent.ucs(map, city_a, city_b)
+            astar_result = agent.astar(map, city_a, city_b)
+            print_results_to_solution_file([bfs_result, dls_result, ucs_result, astar_result])
+
 
 
 if __name__ == '__main__':
